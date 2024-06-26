bae518cba33893a7a90084315594e8232e2949ef
manav-singhal12 <manavsinghal165@gmail.com>
2024-06-26 20:43:42 +0530

    gd

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
@@ -7,21 +8,23 @@ import cookieParser from "cookie-parser";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cors from "cors";
 import { app, server } from "./socket/socket.js";
-
 const PORT = process.env.PORT || 5000;
-
+const __dirname=path.resolve()
 dotenv.config();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-    res.send("Helloo weolsdd");
-});
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname,"frontend/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
