99f85c230259ce9033ea6085629aa7bf0ca059ef
manav-singhal12 <manavsinghal165@gmail.com>
2024-07-31 14:15:28 +0530

    p

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://chatapp-0jtv.onrender.com", {
                 query: {
                     userId: authUser._id,
                 },
